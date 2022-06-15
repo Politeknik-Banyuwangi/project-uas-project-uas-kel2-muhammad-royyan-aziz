85e336d53c39294c1f5ac392f48383ecabd5d360
FlutLab.io <>
2022-06-15 19:16:12 +0000

    Autosave

--- lib/login.dart
+++ lib/login.dart
@@ -23,21 +23,34 @@ class _MyLoginState extends State<MyLogin> {
               style: TextStyle(color: Colors.white, fontSize: 33),
             ),
           ),
-          Container(
-            padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5, right: 35, left: 35),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(fillColor: Colors.grey.shade100, filled: true, hintText: 'Email', border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(fillColor: Colors.grey.shade100, filled: true, hintText: 'Password', border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
-                ),
-              ],
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5, right: 35, left: 35),
+              child: Column(
+                children: [
+                  TextField(
+                    decoration: InputDecoration(fillColor: Colors.grey.shade100, filled: true, hintText: 'Email', border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(fillColor: Colors.grey.shade100, filled: true, hintText: 'Password', border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Sing in',
+                        style: TextStyle(fontSize: 27, fontWeight: FontWeight.w700),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ]),
