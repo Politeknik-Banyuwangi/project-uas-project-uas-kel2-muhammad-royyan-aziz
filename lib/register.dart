0ca3fbbc6fbcd7fd62d692129833a96eb6d68cf9
FlutLab.io <>
2022-06-16 04:21:17 +0000

    Autosave

--- lib/register.dart
+++ lib/register.dart
@@ -25,9 +25,15 @@ class _MyRegisterState extends State<MyRegister> {
           ),
           SingleChildScrollView(
             child: Container(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5, right: 35, left: 20),
+              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5, right: 35, left: 35),
               child: Column(
                 children: [
+                  TextField(
+                    decoration: InputDecoration(fillColor: Colors.grey.shade100, filled: true, hintText: 'Nama', border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
                   TextField(
                     decoration: InputDecoration(fillColor: Colors.grey.shade100, filled: true, hintText: 'Email', border: OutlineInputBorder(borderRadius: BorderRadius.circular(10))),
                   ),
