3e20547aa1f2b00720273eec3997e765a09d928d
FlutLab.io <>
2022-06-16 19:20:48 +0000

    Autosave

--- lib/login.dart
+++ lib/login.dart
@@ -17,9 +17,9 @@ class _MyLoginState extends State<MyLogin> {
         backgroundColor: Colors.transparent,
         body: Stack(children: [
           Container(
-            padding: EdgeInsets.only(left: 35, top: 100),
+            padding: EdgeInsets.only(left: 50, top: 100),
             child: Image(
-              height: 200,
+              height: 180,
               image: AssetImage('assets/logo.png'),
             ),
           ),
