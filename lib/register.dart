d86a248e0ee799734e792d5da9c4b3b3000288ac
FlutLab.io <>
2022-06-18 07:00:34 +0000

    Autosave

--- lib/register.dart
+++ lib/register.dart
@@ -9,101 +9,148 @@ class MyRegister extends StatefulWidget {
 }
 
 class _MyRegisterState extends State<MyRegister> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(image: DecorationImage(image: AssetImage('assets/register.jpeg'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        body: Stack(children: [
-          Container(
-            padding: EdgeInsets.only(left: 40, top: 60),
-            child: Text(
-              'Selamat\nDatang',
-              style: TextStyle(color: Colors.black, fontSize: 45, fontWeight: FontWeight.bold),
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 40, top: 60),
+              child: Text(
+                'Selamat\nDatang',
+                style: TextStyle(color: Colors.black, fontSize: 45, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          Container(
-              padding: EdgeInsets.only(left: 10, top: 200, right: 10),
-              child: GridView.count(crossAxisCount: 2, children: <Widget>[
-                Card(
-                  margin: EdgeInsets.all(8.0),
-                  child: InkWell(
-                    onTap: () {},
-                    splashColor: Colors.yellow,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Icon(Icons.emoji_people, size: 100),
-                          Text(
-                            "Profil",
-                            style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+            Container(
+                padding: EdgeInsets.only(left: 10, top: 200, right: 10),
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  children: <Widget>[
+                    Card(
+                      margin: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.yellow,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(Icons.emoji_people, size: 100),
+                              Text(
+                                "Profil",
+                                style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.all(8.0),
-                  child: InkWell(
-                    onTap: () {},
-                    splashColor: Colors.yellow,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Icon(Icons.qr_code_scanner_rounded, size: 100),
-                          Text(
-                            "Scanner",
-                            style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                    Card(
+                      margin: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.yellow,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(Icons.qr_code_scanner_rounded, size: 100),
+                              Text(
+                                "Scanner",
+                                style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.all(8.0),
-                  child: InkWell(
-                    onTap: () {},
-                    splashColor: Colors.yellow,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Icon(Icons.app_registration, size: 100),
-                          Text(
-                            "Jadwal",
-                            style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                    Card(
+                      margin: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.yellow,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(Icons.app_registration, size: 100),
+                              Text(
+                                "Jadwal",
+                                style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-                Card(
-                  margin: EdgeInsets.all(8.0),
-                  child: InkWell(
-                    onTap: () {},
-                    splashColor: Colors.yellow,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: <Widget>[
-                          Icon(Icons.schedule, size: 100),
-                          Text(
-                            "Kehadiran",
-                            style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                    Card(
+                      margin: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.yellow,
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+                              Icon(Icons.schedule, size: 100),
+                              Text(
+                                "Kehadiran",
+                                style: TextStyle(color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                        ],
+                        ),
                       ),
                     ),
-                  ),
-                ),
-              ])),
-        ]),
+                  ],
+                )),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
