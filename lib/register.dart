785050ba3f054984e1acd98a9b1af0a46a942f6a
FlutLab.io <>
2022-06-18 21:05:23 +0000

    Autosave

--- lib/register.dart
+++ lib/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:projectuas/login.dart';
+import 'package:projectuas/profilescreen.dart';
 
 class MyRegister extends StatefulWidget {
   const MyRegister({Key? key}) : super(key: key);
