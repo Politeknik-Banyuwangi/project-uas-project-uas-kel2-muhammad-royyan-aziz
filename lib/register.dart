3498498944416af10a0b9201e07af4a8d13f7898
FlutLab.io <>
2022-06-18 04:29:53 +0000

    Autosave

--- lib/register.dart
+++ lib/register.dart
@@ -19,127 +19,29 @@ class _MyRegisterState extends State<MyRegister> {
           Container(
             padding: EdgeInsets.only(left: 35, top: 70),
             child: Text(
-              'Buat\nAkun',
+              '\nDatang',
               style: TextStyle(color: Colors.black, fontSize: 45),
             ),
           ),
-          SingleChildScrollView(
-            child: Container(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.4, right: 35, left: 35),
-              child: Column(
-                children: [
-                  TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      hintText: 'Nama',
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+          Container(
+              padding: EdgeInsets.all(30.0),
+              child: GridView.count(crossAxisCount: 2, children: <Widget>[
+                Card(
+                  child: InkWell(
+                    onTap: () {},
+                    splashColor: Colors.yellow,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text("FlutLab is on your service!"),
+                          Icon(Icons.mood),
+                        ],
                       ),
                     ),
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      hintText: 'Email',
-                      hintStyle: TextStyle(color: Colors.black),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                    decoration: InputDecoration(
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        borderSide: BorderSide(color: Colors.white),
-                      ),
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Colors.white),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'Sing in',
-                        style: TextStyle(fontSize: 27, fontWeight: FontWeight.w700),
-                      ),
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundColor: Color(0xff4c505b),
-                        child: IconButton(
-                          color: Colors.white,
-                          onPressed: () {},
-                          icon: Icon(Icons.arrow_forward),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, 'register');
-                          },
-                          child: Text(
-                            'Sing Up',
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontSize: 18,
-                              color: Color(0xff4c505b),
-                            ),
-                          )),
-                      TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Forget Password',
-                            style: TextStyle(
-                              decoration: TextDecoration.underline,
-                              fontSize: 18,
-                              color: Color(0xff4c505b),
-                            ),
-                          )),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+                ),
+              ])),
         ]),
       ),
     );
