0fefb4cce49f1582820e468df9ec3634a223e794
FlutLab.io <>
2022-06-18 04:49:07 +0000

    Autosave

--- lib/register.dart
+++ lib/register.dart
@@ -24,10 +24,10 @@ class _MyRegisterState extends State<MyRegister> {
             ),
           ),
           Container(
-              padding: EdgeInsets.only(left: 35, top: 200),
+              padding: EdgeInsets.only(left: 20, top: 200),
               child: GridView.count(crossAxisCount: 2, children: <Widget>[
                 Card(
-                  margin: EdgeInsets.all(6.0),
+                  margin: EdgeInsets.all(8.0),
                   child: InkWell(
                     onTap: () {},
                     splashColor: Colors.yellow,
