f927c9e2f9e1a71763582bf0d718457fb0a1d0dd
FlutLab.io <>
2022-06-21 18:26:34 +0000

    Autosave

--- lib/cekpresensi.dart
+++ lib/cekpresensi.dart
@@ -100,7 +100,7 @@ class _CekPresensiState extends State<CekPresensi> {
                           fontWeight: FontWeight.w400,
                         ),
                       ),
-                      SizedBox(width: 25.0),
+                      SizedBox(width: 30.0),
                       Icon(
                         Icons.keyboard_arrow_right,
                         color: Colors.white,
